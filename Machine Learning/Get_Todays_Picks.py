165dfe6c9ae73a2e6def04a9c342c8e3c85f6650
mhsmathew <32316796+mhsmathew@users.noreply.github.com>
2020-04-24 23:20:16 -0400

    Format fix

--- Machine Learning/Get_Todays_Picks.py
+++ Machine Learning/Get_Todays_Picks.py
@@ -2,12 +2,14 @@ from Stock_List import Access_Tickers
 from Get_Data import GetData
 import pandas as pd
 
-tickers = Access_Tickers().get_stocks()
-data_frames = []
-for ticker in tickers:
-    print(ticker)
-    t=GetData(ticker, True).prepare_data_for_training()
-    t['ticker']=ticker
-    data_frames.append(t)
-    # Will add
-pd.concat(data_frames, ignore_index=True).to_csv('today.csv', index=True)
+def todays_stock_data():
+    tickers = Access_Tickers().get_stocks()
+    data_frames = []
+    for ticker in tickers:
+        t = GetData(ticker, True).prepare_data_for_training()
+        t['ticker'] = ticker
+        # Date and short_result are not needed
+        t = t[t.columns.drop('date')]
+        t = t[t.columns.drop('short_result')]
+        data_frames.append(t)
+    return pd.concat(data_frames)
